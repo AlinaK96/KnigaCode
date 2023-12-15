bc2a45ab6f974584488147a036d08e2e022a6ce2
Alina <AliKochneva@yandex.ru>
2023-12-15 13:07:53 +0700

    search

--- src/components/pages/Support.jsx
+++ src/components/pages/Support.jsx
@@ -48,12 +48,12 @@ const Support = () => {
             <Header />
             <div className="content">
                 <InfoHead content='Мы подготовили для вас ответы на самые часто задаваемые вопросы' />
-                <div>
+                <div className={classes.header__addBtn}>
                     <Input 
                         type='search'
                         placeholder='Поиск...'
                     />
-                    <a href="#!">Задать вопрос</a>
+                    <a href="https://you.com/" target="blank" className="customLink">Задать вопрос</a>
                 </div>
 
                 <div className="book">
