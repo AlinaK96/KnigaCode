14420ed59c60624fad744ff5238ed74866d8bddd
Alina <AliKochneva@yandex.ru>
2023-12-26 15:11:31 +0700

    video

--- src/components/blocks/video/VideoBlock.jsx
+++ src/components/blocks/video/VideoBlock.jsx
@@ -2,12 +2,14 @@ import React from "react"
 
 const VideoBlock = ({video}) => {
 
+    const url = `https://www.youtube.com/embed/${video.URL}`; 
+
         return (
             <div style={{width: '100%'}}>
-                <h3>{video.title}</h3> 
+                <h3 style={{textAlign: 'center'}} >{video.title}</h3> 
                 <div>
-                    {video.URL}
-                    {video.description} 
+                <iframe style={{float: 'left', margin: '10px'}} width="450" height="300" src={url} frameBorder="0" allowFullScreen></iframe>
+                <p>{video.description} </p>
                 </div>
             </div>
         )
